c5aa338c2b2d502b01ee8454307ba637bc9b2de6
eliasblume <mail@elias-blu.me>
2023-01-12 20:04:08 +0100

    test update feature

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import product from './pages/product'
 import shop from './pages/shop'
 ;(async () => {
     console.log('pandabuy-userscript ' + process.env.USERSCRIPT_VERSION)
+
     const pageType = await findType()
     if (pageType === PageType.PRODUCT) await product()
     if (pageType === PageType.SHOP) await shop()
